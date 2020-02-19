9a61fdb11c838a0c53f6dd49f0b9d47fb9ae3f4f
Shuheng Johnny Liu <wish1104@outlook.com>
2020-02-20 00:36:01 +0800

    fix a bug that could render any file as html

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ var server = http.createServer(function(req, res) {
         handleError(err, res);
         return;
     } else {
-        res.setHeader('Content-Type', 'text/html');
         res.end(data);
     }
   });
