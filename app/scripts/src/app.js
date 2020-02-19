bccf8ce4586f097c976f838271f61a2f3c1c964b
Shuheng Johnny Liu <wish1104@outlook.com>
2020-02-19 22:05:10 +0800

    implement logic for client websocket

---
+++ app/scripts/src/app.js
@@ -0,0 +1,38 @@
+import socket from './ws-client';
+
+class ChatApp {
+    constructor() {
+        socket.init('ws://localhost:3001');
+
+        socket.registerOpenHandler(() => {
+            let message = new ChatMessage({message: 'pow!'});
+            socket.sendMessage(message.serialize());
+        });
+
+        socket.registerMessageHandler((data) => {
+            console.log(data);
+        });
+    }
+}
+
+class ChatMessage {
+    constructor({
+        message: m,
+        user: u='batman',
+        timestamp: t=(new Date()).getTime()
+    }) {
+        this.message = m;
+        this.user = u;
+        this.timestamp = t;
+    }
+
+    serialize() {
+        return {
+            message: this.message,
+            user: this.user,
+            timestamp: this.timestamp
+        };
+    }
+}
+
+export default ChatApp;
---
+++ app/scripts/src/main.js
@@ -0,0 +1,3 @@
+import ChatApp from "./app";  // `ChatApp` can be substituted for other names
+
+new ChatApp();
